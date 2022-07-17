7d598fe415aed580ee6f52757ff1643ed267e438
Alina Voicu <a.voicu24@gmail.com>
2022-07-17 15:01:21 +0300

    Basic app structure

---
+++ src/features/restaurants/components/restaurant-info.component.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+import { Card } from "react-native-paper";
+
+export const RestaurantInfo = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon,
+    photos = [
+      "https://images.unsplash.com/photo-1414235077428-338989a2e8c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80",
+    ],
+    address = "100 Andrew Street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
+  return (
+    <Card elevation={5} style={styles.card}>
+      <Card.Cover key={name} style={styles.cover} source={{ uri: photos[0] }} />
+      <Card.Content>
+        <Text style={styles.title}>{name}</Text>
+      </Card.Content>
+    </Card>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "white",
+  },
+  cover: {
+    padding: 20,
+    backgroundColor: "white",
+  },
+  title: {
+    padding: 16,
+  },
+});
