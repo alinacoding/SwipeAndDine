376c77cb3351e3f2f9c5408dcd065c3ace3c46eb
Alina Voicu <a.voicu24@gmail.com>
2022-07-23 16:30:46 +0300

    Camelize the results

--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,4 +1,6 @@
 import { mocks } from "./mock";
+import camelize from "camelize";
+
 export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
   return new Promise((resolve, reject) => {
     const mock = mocks[location];
@@ -10,7 +12,7 @@ export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
 };
 restaurantsRequest()
   .then((result) => {
-    console.log(result);
+    console.log(camelize(result));
   })
   .catch((err) => {
     console.log(err);
