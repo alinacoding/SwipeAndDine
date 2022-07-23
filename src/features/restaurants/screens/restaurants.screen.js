04a12f47830b3c465e8881fd170f786c4f4b063c
Alina Voicu <a.voicu24@gmail.com>
2022-07-23 19:41:12 +0300

    Adding ActivityIndicator upon loading the app

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,11 +1,12 @@
 import React, { useContext } from "react";
+import { View } from "react-native";
 import { StatusBar, SafeAreaView, FlatList } from "react-native";
 import { Searchbar } from "react-native-paper";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 import styled from "styled-components/native";
 import { SafeArea } from "../../../../src/components/utility/safe-area.component";
-import { Spacer } from "../../../components/spacer/spacer.component";
-
+// import { Spacer } from "../../../components/spacer/spacer.component";
+import { ActivityIndicator, Colors } from "react-native-paper";
 import { RestaurantContext } from "../../../services/restaurants/restaurants.context";
 
 const SearchContainer = styled.View`
@@ -18,10 +19,29 @@ const RestaurantList = styled.FlatList.attrs({
   },
 })``;
 
+const Loading = styled(ActivityIndicator)`
+  margin-left=-25px;
+`;
+
+const LoadingContainer = styled.View`
+  position: absolute;
+  topL 50%;
+  left: 50%;
+`;
+
 export const RestaurantsScreen = () => {
   const { isLoading, error, restaurants } = useContext(RestaurantContext);
   return (
     <SafeArea>
+      {isLoading && (
+        <LoadingContainer>
+          <Loading>
+            size={50}
+            animating={true}
+            colors={Colors.blue300}
+          </Loading>
+        </LoadingContainer>
+      )}
       <SearchContainer>
         <Searchbar />
       </SearchContainer>
