d75d621a7938006de4359d89a4e9ed7e055a6f41
Alina Voicu <a.voicu24@gmail.com>
2022-07-23 14:50:08 +0300

    Mimicking API call

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Text } from "react-native";
 import { SafeArea } from "./src/components/utility/safe-area.component";
 import { Ionicons } from "@expo/vector-icons";
-
+import { restaurantsRequest } from "./src/services/restaurants/restaurants.service";
 import {
   useFonts as useOswald,
   Oswald_400Regular,
